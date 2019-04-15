afea02b32368921904e98771e467e899d6d59f0f
Conor Casey <conorcaseyc@icloud.com>
2019-04-15 20:24:06 +0100

    Optimised code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AMSIMP",
-    version="0.0.1",
+    version="0.0.2",
     author="Conor Casey",
     author_email="conorcaseyc@icloud.com",
     description="Model for Atmospheric Dynamics",
