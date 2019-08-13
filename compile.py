5a82561670628b63c84e64a3b26a7a1590d72f88
Conor Casey <conorcaseyc@icloud.com>
2019-08-13 02:08:35 +0100

    AMSIMP v0.2.4

--- compile.py
+++ compile.py
@@ -1,8 +1,13 @@
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 import numpy
 
 ext_modules = [
+    Extension("amsimp.backend", ["amsimp/backend.pxd"]),
+    Extension("amsimp.wind", ["amsimp/wind.pxd"]),
+    Extension("amsimp.water", ["amsimp/water.pxd"]),
+    Extension("amsimp.weather", ["amsimp/weather.pxd"]),
     Extension("amsimp.backend", ["amsimp/backend.pyx"]),
     Extension("amsimp.wind", ["amsimp/wind.pyx"]),
     Extension("amsimp.water", ["amsimp/water.pyx"]),
@@ -12,6 +17,6 @@ ext_modules = [
 setup(
     name = 'amsimp',
     cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules,
+    ext_modules = cythonize(ext_modules),
     include_dirs=[numpy.get_include()],
 )
