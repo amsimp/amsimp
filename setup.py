df57e6cb0e7ae68748a9bd2aa3641cf205aeab16
Conor Casey <conorcaseyc@icloud.com>
2019-11-07 18:18:13 -0700

    AMSIMP v0.3.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     name="amsimp",
     ext_modules=extensions,
     include_dirs=[numpy.get_include()],
-    version="0.3.1",
+    version="0.3.2",
     author="Conor Casey",
     author_email="support@amsimp.com",
     description="Simulator for Atmospheric Dynamics",
@@ -65,7 +65,8 @@ setup(
         "numpy",
         "cartopy",
         "pandas",
-        "wget",
+        "python-wget",
+        "pynverse",
     ],
     zip_safe=False,
 )
