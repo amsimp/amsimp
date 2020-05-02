524baccb0feb94c66e57c66824a8ebd2f7ed19b5
Conor Casey <conorcaseyc@icloud.com>
2020-05-02 15:04:08 +0100

    Update benchmarking.py

--- benchmarking.py
+++ benchmarking.py
@@ -239,9 +239,11 @@ def benchmarking():
             else:
                 # Geopotential Height.
                 height = height[0]
+                print(np.shape(height))
                 # Geostrophic Wind.
                 # Zonal Wind.
                 u = u[0]
+                print(np.shape(u))
                 # Meridional Wind.
                 v = v[0]
                 # Temperature.
