52efda7631a6ff0218892832bfe32a2e156710cb
Conor Casey <conorcaseyc@icloud.com>
2020-10-04 11:58:55 +0100

    Update download_gfs.py

--- benchmarking/scifest-national/download_gfs.py
+++ benchmarking/scifest-national/download_gfs.py
@@ -30,7 +30,7 @@ file_locations = [
 ]
 
 # Define the start date.
-date = datetime(2019, 1, 1, 0)
+date = datetime(2019, 2, 4, 0)
 end = datetime(2020, 1, 1, 0)
 diff = end - date
 days_diff = diff.days
