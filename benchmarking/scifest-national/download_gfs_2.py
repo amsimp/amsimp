396d968ba619bc2fe06811760500f0aeaffe2812
Conor Casey <conorcaseyc@icloud.com>
2020-10-10 13:14:51 +0100

    Update download_gfs_2.py

--- benchmarking/scifest-national/download_gfs_2.py
+++ benchmarking/scifest-national/download_gfs_2.py
@@ -158,6 +158,9 @@ for i in range(it):
             # Load file.
             data = iris.load(data_file)
 
+            # Remove file.
+            os.remove(data_file)
+
             # Retrieve temperature, wind, relative humidity, and geopotential height data.
             temperature = data.extract('air_temperature')[0]
             rh = data.extract('relative_humidity')[0]
