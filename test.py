f7000e0fff0fdb9f27113b995e4d0899ae4e8210
Conor Casey <conorcaseyc@icloud.com>
2021-01-03 13:40:23 +0000

    Updated README.md

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 # Import dependicies.
 import amsimp
+import iris
 
-# Define model.
-model = amsimp.OperationalModel()
+# Define the operational model and the forecast length.
+model = amsimp.OperationalModel(forecast_length=120)
 
-# Generate sample forecast.
+# # Generate a near real-time weather forecast.
 fct = model.generate_forecast()
+
+# Save forecast.
+iris.save(fct, 'gfm_example.nc')
